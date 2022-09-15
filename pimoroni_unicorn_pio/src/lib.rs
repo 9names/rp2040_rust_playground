812ea253baf9a8bf535a96582183bb7e5462abfb
9names <60134748+9names@users.noreply.github.com>
2022-09-15 23:01:10 +1000

    Basic e-g impl in unicorn

--- pimoroni_unicorn_pio/src/lib.rs
+++ pimoroni_unicorn_pio/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use core::convert::TryInto;
+
 use bsp::hal::{
     gpio::{bank0::*, DynPin, FunctionConfig, FunctionPio0, Pin},
     pio::{
@@ -47,7 +49,7 @@ pub struct UnicornPins {
 
 pub struct UnicornDynPins([DynPin; 11]);
 
-/// Instance of I2C Controller.
+/// Instance of Pico Unicorn display
 pub struct Unicorn<'pio, P, SM>
 where
     P: PIOExt + FunctionConfig,
@@ -209,14 +211,6 @@ where
         .program
     }
 
-    pub fn draw(&mut self) {
-        for batch in unsafe { BITSTREAM.chunks_exact(4) } {
-            while !self.tx.write(u32::from_le_bytes(unsafe {
-                batch.try_into().unwrap_unchecked()
-            })) {}
-        }
-    }
-
     pub fn init_bitstream_rs() {
         // initialise the bcd timing values and row selects in the bitstream
         for row in 0..HEIGHT {
@@ -229,7 +223,7 @@ where
                 let bcd_offset = offset + 10;
 
                 // the last bcd frame is used to allow the fets to discharge to avoid ghosting
-                if (frame == BCD_FRAMES - 1usize) {
+                if frame == BCD_FRAMES - 1usize {
                     let bcd_ticks: u16 = 65535;
                     unsafe {
                         BITSTREAM[row_select_offset] = 0b11111111;
@@ -251,8 +245,44 @@ where
             }
         }
     }
+}
 
-    pub fn set_pixel(&mut self, x: u8, y: u8, r: u8, g: u8, b: u8) {
+static GAMMA_14BIT: [u16; 256] = [
+    0, 0, 0, 1, 2, 3, 4, 6, 8, 10, 13, 16, 20, 23, 28, 32, 37, 42, 48, 54, 61, 67, 75, 82, 90, 99,
+    108, 117, 127, 137, 148, 159, 170, 182, 195, 207, 221, 234, 249, 263, 278, 294, 310, 326, 343,
+    361, 379, 397, 416, 435, 455, 475, 496, 517, 539, 561, 583, 607, 630, 654, 679, 704, 730, 756,
+    783, 810, 838, 866, 894, 924, 953, 983, 1014, 1045, 1077, 1110, 1142, 1176, 1210, 1244, 1279,
+    1314, 1350, 1387, 1424, 1461, 1499, 1538, 1577, 1617, 1657, 1698, 1739, 1781, 1823, 1866, 1910,
+    1954, 1998, 2044, 2089, 2136, 2182, 2230, 2278, 2326, 2375, 2425, 2475, 2525, 2577, 2629, 2681,
+    2734, 2787, 2841, 2896, 2951, 3007, 3063, 3120, 3178, 3236, 3295, 3354, 3414, 3474, 3535, 3596,
+    3658, 3721, 3784, 3848, 3913, 3978, 4043, 4110, 4176, 4244, 4312, 4380, 4449, 4519, 4589, 4660,
+    4732, 4804, 4876, 4950, 5024, 5098, 5173, 5249, 5325, 5402, 5479, 5557, 5636, 5715, 5795, 5876,
+    5957, 6039, 6121, 6204, 6287, 6372, 6456, 6542, 6628, 6714, 6801, 6889, 6978, 7067, 7156, 7247,
+    7337, 7429, 7521, 7614, 7707, 7801, 7896, 7991, 8087, 8183, 8281, 8378, 8477, 8576, 8675, 8775,
+    8876, 8978, 9080, 9183, 9286, 9390, 9495, 9600, 9706, 9812, 9920, 10027, 10136, 10245, 10355,
+    10465, 10576, 10688, 10800, 10913, 11027, 11141, 11256, 11371, 11487, 11604, 11721, 11840,
+    11958, 12078, 12198, 12318, 12440, 12562, 12684, 12807, 12931, 13056, 13181, 13307, 13433,
+    13561, 13688, 13817, 13946, 14076, 14206, 14337, 14469, 14602, 14735, 14868, 15003, 15138,
+    15273, 15410, 15547, 15685, 15823, 15962, 16102, 16242, 16383,
+];
+
+impl<'pio, P, SM> Unicorn<'pio, P, SM>
+where
+    P: PIOExt + FunctionConfig,
+    SM: ValidStateMachine<PIO = P>,
+{
+    #[inline(always)]
+    pub fn set_pixel(&mut self, coord: Point, color: Rgb888) {
+        self.set_pixel_rgb(
+            coord.x as u8,
+            coord.y as u8,
+            color.r(),
+            color.g(),
+            color.b(),
+        )
+    }
+
+    pub fn set_pixel_rgb(&mut self, x: u8, y: u8, r: u8, g: u8, b: u8) {
         let x = x as usize;
         let y = y as usize;
         if x >= WIDTH || y >= HEIGHT {
@@ -299,23 +329,53 @@ where
             gb >>= 1;
         }
     }
+
+    pub fn draw(&mut self) {
+        for batch in unsafe { BITSTREAM.chunks_exact(4) } {
+            while !self.tx.write(u32::from_le_bytes(unsafe {
+                batch.try_into().unwrap_unchecked()
+            })) {}
+        }
+    }
 }
 
-static GAMMA_14BIT: [u16; 256] = [
-    0, 0, 0, 1, 2, 3, 4, 6, 8, 10, 13, 16, 20, 23, 28, 32, 37, 42, 48, 54, 61, 67, 75, 82, 90, 99,
-    108, 117, 127, 137, 148, 159, 170, 182, 195, 207, 221, 234, 249, 263, 278, 294, 310, 326, 343,
-    361, 379, 397, 416, 435, 455, 475, 496, 517, 539, 561, 583, 607, 630, 654, 679, 704, 730, 756,
-    783, 810, 838, 866, 894, 924, 953, 983, 1014, 1045, 1077, 1110, 1142, 1176, 1210, 1244, 1279,
-    1314, 1350, 1387, 1424, 1461, 1499, 1538, 1577, 1617, 1657, 1698, 1739, 1781, 1823, 1866, 1910,
-    1954, 1998, 2044, 2089, 2136, 2182, 2230, 2278, 2326, 2375, 2425, 2475, 2525, 2577, 2629, 2681,
-    2734, 2787, 2841, 2896, 2951, 3007, 3063, 3120, 3178, 3236, 3295, 3354, 3414, 3474, 3535, 3596,
-    3658, 3721, 3784, 3848, 3913, 3978, 4043, 4110, 4176, 4244, 4312, 4380, 4449, 4519, 4589, 4660,
-    4732, 4804, 4876, 4950, 5024, 5098, 5173, 5249, 5325, 5402, 5479, 5557, 5636, 5715, 5795, 5876,
-    5957, 6039, 6121, 6204, 6287, 6372, 6456, 6542, 6628, 6714, 6801, 6889, 6978, 7067, 7156, 7247,
-    7337, 7429, 7521, 7614, 7707, 7801, 7896, 7991, 8087, 8183, 8281, 8378, 8477, 8576, 8675, 8775,
-    8876, 8978, 9080, 9183, 9286, 9390, 9495, 9600, 9706, 9812, 9920, 10027, 10136, 10245, 10355,
-    10465, 10576, 10688, 10800, 10913, 11027, 11141, 11256, 11371, 11487, 11604, 11721, 11840,
-    11958, 12078, 12198, 12318, 12440, 12562, 12684, 12807, 12931, 13056, 13181, 13307, 13433,
-    13561, 13688, 13817, 13946, 14076, 14206, 14337, 14469, 14602, 14735, 14868, 15003, 15138,
-    15273, 15410, 15547, 15685, 15823, 15962, 16102, 16242, 16383,
-];
+#[cfg(feature = "graphics")]
+use embedded_graphics_core::{
+    draw_target::DrawTarget,
+    geometry::Size,
+    geometry::{Dimensions, OriginDimensions},
+    pixelcolor::Rgb888,
+    prelude::*,
+};
+
+#[cfg(feature = "graphics")]
+impl<'pio, P, SM> DrawTarget for Unicorn<'pio, P, SM>
+where
+    P: PIOExt + FunctionConfig,
+    SM: ValidStateMachine<PIO = P>,
+{
+    type Color = Rgb888;
+    type Error = core::convert::Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        let bb = self.bounding_box();
+        pixels
+            .into_iter()
+            .filter(|Pixel(pos, _color)| bb.contains(*pos))
+            .for_each(|Pixel(pos, color)| self.set_pixel(pos, color));
+        Ok(())
+    }
+}
+
+impl<'pio, P, SM> OriginDimensions for Unicorn<'pio, P, SM>
+where
+    P: PIOExt + FunctionConfig,
+    SM: ValidStateMachine<PIO = P>,
+{
+    fn size(&self) -> Size {
+        Size::new(WIDTH as u32, HEIGHT as u32)
+    }
+}
--- pimoroni_unicorn_pio/src/main.rs
+++ pimoroni_unicorn_pio/src/main.rs
@@ -8,6 +8,7 @@
 use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
+use embedded_graphics_core::pixelcolor::Rgb888;
 use embedded_hal::digital::v2::ToggleableOutputPin;
 use panic_probe as _;
 use pimoroni_unicorn_pio::UnicornPins;
@@ -15,17 +16,23 @@ use pimoroni_unicorn_pio::UnicornPins;
 use bsp::hal::{clocks::init_clocks_and_plls, pac, pio::PIOExt, sio::Sio, watchdog::Watchdog};
 use rp_pico as bsp;
 
+use embedded_graphics::{
+    pixelcolor::RgbColor,
+    prelude::*,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
+};
+
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    let _core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
+    let _clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
@@ -61,25 +68,37 @@ fn main() -> ! {
 
     let mut uni = pimoroni_unicorn_pio::Unicorn::new(&mut pio, sm0, unipins);
     // Buttons
-    let mut btn_a = pins.gpio12.into_floating_input();
-    let mut btn_b = pins.gpio13.into_floating_input();
-    let mut btn_x = pins.gpio14.into_floating_input();
-    let mut btn_y = pins.gpio15.into_floating_input();
-
-    let loop_update_freq = 1000;
-    let mut loop_counter = 0;
+    let mut _btn_a = pins.gpio12.into_floating_input();
+    let mut _btn_b = pins.gpio13.into_floating_input();
+    let mut _btn_x = pins.gpio14.into_floating_input();
+    let mut _btn_y = pins.gpio15.into_floating_input();
 
     let mut led_pin = pins.led.into_push_pull_output();
 
     let _ = led_pin.toggle();
+    Rectangle::new(Point::new(1, 5), Size::new(2, 3))
+        .into_styled(
+            PrimitiveStyleBuilder::new()
+                .stroke_color(Rgb888::RED)
+                .stroke_width(5)
+                .fill_color(Rgb888::GREEN)
+                .build(),
+        )
+        .draw(&mut uni)
+        .unwrap();
     loop {
-        let colours = [(255, 0, 0), (0, 255, 0), (0, 0, 255)];
-        for color in colours {
-            for y in 0..pimoroni_unicorn_pio::HEIGHT as u8 {
-                for x in 0..pimoroni_unicorn_pio::WIDTH as u8 {
-                    uni.set_pixel(x, y, color.0, color.1, color.2);
+        let colours = [
+            Rgb888::new(255, 0, 0),
+            Rgb888::new(0, 255, 0),
+            Rgb888::new(0, 0, 255),
+        ];
+        let clear = Rgb888::new(0, 0, 0);
+        for colour in colours {
+            for y in 0..pimoroni_unicorn_pio::HEIGHT as i32 {
+                for x in 0..pimoroni_unicorn_pio::WIDTH as i32 {
+                    uni.set_pixel(Point::new(x, y), colour);
                     uni.draw();
-                    uni.set_pixel(x, y, 0, 0, 0);
+                    uni.set_pixel(Point::new(x, y), clear);
                 }
             }
         }
